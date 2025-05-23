0f7cc65d3284bf7c148cb3e64dc868c8176c10e3
stpdcybersec <Kunegin.aleksey@ya.ru>
2025-05-23 11:01:15 +0700

    Delete Lab1 directory

--- Lab1/Motorcycle.py
+++
@@ -1,9 +0,0 @@
-from Ride import Ride
-
-class Motorcycle(Ride):
-    def __init__(self, date, plate):
-        super().__init__(date)
-        self.__plate = plate
-
-    def __str__(self):
-        return f"Type: Motorcycle, Date: {self.date.date()}, Plate: {self.__plate}"
